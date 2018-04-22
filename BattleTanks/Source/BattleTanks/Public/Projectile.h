912ad1430b2277fd7c2763bd11c88252fbf5a2ba
Joshua Wood <joshuawood345@gmail.com>
2018-04-22 12:14:43 +0100

    Projectile Movement Component

--- BattleTanks/Source/BattleTanks/Public/Projectile.h
+++ BattleTanks/Source/BattleTanks/Public/Projectile.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Runtime/Engine/Classes/GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
 
 UCLASS()
@@ -12,17 +13,17 @@ class BATTLETANKS_API AProjectile : public AActor
 	GENERATED_BODY()
 	
 public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 	// Sets default values for this actor's properties
 	AProjectile();
+
+	void LaunchProjectile(float Speed);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	
-	
+	UProjectileMovementComponent * ProjectileMovement = nullptr;
 };
