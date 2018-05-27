dd1eb57143b4d1320ae86b0c92c9457e87f1808e
Joshua Wood <joshuawood345@gmail.com>
2018-05-27 13:58:17 +0100

    Completing Manual Tank Movement

--- BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -1,24 +1,43 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "TankTrack.h"
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	// auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
-
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 	// TODO prevent double speed due to dual control use
 }
 
+void UTankMovementComponent::IntendMoveBackwards(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(-Throw);
+	RightTrack->SetThrottle(-Throw);
+	// TODO prevent double speed due to dual control use
+}
 
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+	// TODO prevent double speed due to dual control use
+}
+
+void UTankMovementComponent::IntendTurnLeft(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(-Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO prevent double speed due to dual control use
+}
--- BattleTanks/Source/BattleTanks/Public/TankMovementComponent.h
+++ BattleTanks/Source/BattleTanks/Public/TankMovementComponent.h
@@ -22,6 +22,15 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendMoveBackwards(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnLeft(float Throw);
 	
 private:
 	UTankTrack* LeftTrack = nullptr;
