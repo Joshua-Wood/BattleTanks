a47e6166b13acdab91a1c0c94dbb3d43a4624403
Joshua Wood <joshuawood345@gmail.com>
2018-05-28 17:02:32 +0100

    Finalising Your Class Code

--- BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -22,8 +22,6 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 
 	auto RightThrow = FVector::CrossProduct(AIForwardIntention, TankForward).Z;
 	IntendTurnRight(RightThrow);
-
-	// UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *AIForwardIntention)
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
@@ -31,15 +29,6 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-	// TODO prevent double speed due to dual control use
-}
-
-void UTankMovementComponent::IntendMoveBackwards(float Throw)
-{
-	if (!LeftTrack || !RightTrack) { return; }
-	LeftTrack->SetThrottle(-Throw);
-	RightTrack->SetThrottle(-Throw);
-	// TODO prevent double speed due to dual control use
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
@@ -47,13 +36,4 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-	// TODO prevent double speed due to dual control use
-}
-
-void UTankMovementComponent::IntendTurnLeft(float Throw)
-{
-	if (!LeftTrack || !RightTrack) { return; }
-	LeftTrack->SetThrottle(-Throw);
-	RightTrack->SetThrottle(Throw);
-	// TODO prevent double speed due to dual control use
 }
